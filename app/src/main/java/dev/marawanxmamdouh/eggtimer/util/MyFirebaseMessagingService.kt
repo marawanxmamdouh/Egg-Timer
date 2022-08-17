7f3c25c29468c85558b019472cf7ec5cccffd6ab
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-17 14:06:10 +0200

    Handle data payload messages

--- app/src/main/java/dev/marawanxmamdouh/eggtimer/util/MyFirebaseMessagingService.kt
+++ app/src/main/java/dev/marawanxmamdouh/eggtimer/util/MyFirebaseMessagingService.kt
@@ -4,8 +4,16 @@ import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+private const val TAG = "MyFirebaseMsgService"
+
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.i(TAG, "From: ${remoteMessage.from}") // From: /topics/breakfast
+
+        // Check if message contains a data payload.
+        remoteMessage.data.let {
+            Log.i(TAG, "Message data payload: " + remoteMessage.data) // Message data payload: {egg=3}
+        }
         super.onMessageReceived(remoteMessage)
     }
 
