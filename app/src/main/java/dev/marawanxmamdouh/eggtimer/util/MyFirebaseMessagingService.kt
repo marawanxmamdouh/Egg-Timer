50896432a21d473d0f790c7789533a04d587dc1e
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-17 14:19:26 +0200

    Show FCM in Foreground

--- app/src/main/java/dev/marawanxmamdouh/eggtimer/util/MyFirebaseMessagingService.kt
+++ app/src/main/java/dev/marawanxmamdouh/eggtimer/util/MyFirebaseMessagingService.kt
@@ -1,12 +1,16 @@
 package dev.marawanxmamdouh.eggtimer.util
 
+import android.app.NotificationManager
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 private const val TAG = "MyFirebaseMsgService"
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.i(TAG, "From: ${remoteMessage.from}") // From: /topics/breakfast
 
@@ -14,11 +18,21 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         remoteMessage.data.let {
             Log.i(TAG, "Message data payload: " + remoteMessage.data) // Message data payload: {egg=3}
         }
-        super.onMessageReceived(remoteMessage)
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body!!)
+        }
     }
 
     override fun onNewToken(token: String) {
         Log.i("TAG", "Refreshed token: $token")
         super.onNewToken(token)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun sendNotification(messageBody: String) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
     }
 }
